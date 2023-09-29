c76cb81c72929071bc67f5949dd2e7c02cd42ee7
Tzubast <nurazkarahadian@gmail.com>
2023-09-29 19:20:48 +0700

    remove dan edit

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,17 +1,15 @@
-<template lang="">
+<template>
     <div class="container">
         <div class="container-teks">
             Welcome To Our Store!
             <div>
                 <button @click="showAddProductForm = true" class="btn btn-success">Add Product</button>
-                
             </div>
-            
         </div>
 
-            <!-- Jika klik add product -->
-            <div v-if="showAddProductForm" class="add-product-form">
-                <form @submit.prevent="addProduct">
+        <!-- Jika klik add product -->
+        <div v-if="showAddProductForm" class="add-product-form">
+            <form @submit.prevent="addOrUpdateProduct">
                 <div class="form-group">
                     <label for="productName">Product Name</label>
                     <input v-model="newProduct.name" type="text" class="form-control" id="productName" required>
@@ -19,8 +17,8 @@
                 <div class="form-group">
                     <label for="productType">Product Type</label>
                     <select v-model="newProduct.type" class="form-control" id="productType">
-                    <option value="Makanan">Makanan</option>
-                    <option value="Pakaian">Pakaian</option>
+                        <option value="Makanan">Makanan</option>
+                        <option value="Pakaian">Pakaian</option>
                     </select>
                 </div>
                 <div class="form-group">
@@ -31,42 +29,42 @@
                     <label for="productQuantity">Product Quantity</label>
                     <input v-model.number="newProduct.quantity" type="number" class="form-control" id="productQuantity" required>
                 </div>
-                <button type="submit" class="btn btn-primary">Add</button>
-                </form>
-            </div>
-
+                <button type="submit" class="btn btn-primary">{{ editingIndex !== null ? 'Save' : 'Add' }}</button>
+            </form>
+        </div>
 
         <div class="container-card">
-                    <table class="table-auto">
-            <thead>
-                <tr>
-                <th>Nama</th>
-                <th>Tipe</th>
-                <th>Harga</th>
-                <th>Jumlah</th>
-                <th>Action</th>
-
-                </tr>
-            </thead>
-            <tbody>
-                <tr  v-for="(product, index) in products" :key="index">
-                <td>{{ product.name }}</td>
-                <td>{{ product.type }}</td>
-                <td>{{ product.price }}</td>
-                <td>{{ product.quantity }}</td>
-                <td>
-                    <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                    </div>
-                </td>
-               
-                </tr>
-            </tbody>
+            <table class="table-auto">
+                <thead>
+                    <tr>
+                        <th>Nama</th>
+                        <th>Tipe</th>
+                        <th>Harga</th>
+                        <th>Jumlah</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(product, index) in products" :key="index">
+                        <td>{{ product.name }}</td>
+                        <td>{{ product.type }}</td>
+                        <td>{{ product.price }}</td>
+                        <td>{{ product.quantity }}</td>
+                        <td>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                            </div>
+                        </td>
+                        <td>
+                            <button @click="editProduct(index)" class="btn btn-primary">Edit</button>
+                            <button @click="deleteProduct(index)" class="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     </div>
 </template>
-
 
 
 <style>
@@ -130,25 +128,31 @@
 </style>
 
 <script>
-        export default {
-        data() {
-            return {
-        products: [], // Data produk
-        showAddProductForm: false, // Status tampilan form tambah produk
-        newProduct: {
-            name: "",
-            type: "",
-            price: 0,
-            quantity: 0,
-                    },
-                   };
-                },
-        methods: {
-            // ...
-            addProduct() {
-            // Tambahkan produk baru ke dalam array
-            this.products.push({ ...this.newProduct });
-            
+export default {
+    data() {
+        return {
+            products: [], // Data produk
+            showAddProductForm: false, // Status tampilan form tambah produk
+            newProduct: {
+                name: "",
+                type: "",
+                price: 0,
+                quantity: 0,
+            },
+            editingIndex: null, // Indeks produk yang sedang diedit
+        };
+    },
+    methods: {
+        addOrUpdateProduct() {
+            if (this.editingIndex !== null) {
+                // Mengganti data produk yang lama dengan data produk yang telah diedit
+                this.products.splice(this.editingIndex, 1, { ...this.newProduct });
+                this.editingIndex = null;
+            } else {
+                // Tambahkan produk baru ke dalam array
+                this.products.push({ ...this.newProduct });
+            }
+
             // Reset data produk baru
             this.newProduct = {
                 name: "",
@@ -157,10 +161,21 @@
                 quantity: 0,
             };
 
-            // Tutup form setelah produk ditambahkan
+            // Tutup form setelah produk ditambahkan atau diedit
             this.showAddProductForm = false;
-            },
         },
-        // ...  
-        }
+        editProduct(index) {
+            // Mengisi data produk baru dengan data produk yang akan diedit
+            this.newProduct = { ...this.products[index] };
+            // Menyimpan indeks produk yang sedang diedit
+            this.editingIndex = index;
+            // Menampilkan form tambah produk
+            this.showAddProductForm = true;
+        },
+        deleteProduct(index) {
+            // Menghapus produk dari array berdasarkan indeks
+            this.products.splice(index, 1);
+        },
+    },
+};
 </script>
